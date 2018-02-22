3338396160b8cf0a119ef9eab2f42581778b817b
Peter Schrammen <b.schrammen@gmail.com>
2018-02-22 15:08:31 +0100

    initialize player

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Title from './components/Title'
+import Player from './components/Player'
+
+const players = [
+  { id: 1, name: 'Billy', score: 4 },
+  { id: 2, name: 'Safidy', score: 6 },
+  { id: 3, name: 'Iuliia', score: 3 },
+  { id: 4, name: 'Oksana', score: 4 },
+  { id: 5, name: 'Friedrich', score: 5 },
+  { id: 6, name: 'Sia', score: 3 }
+]
+
 
 class App extends Component {
+
   render() {
     return (
-      <div>
-                <Title content="Scoreboard"/>
+      <div className="App">
+        <Title content="Scoreboard"/>
+        <ul>
+          {players.map((player, index) => <Player key={index} { ...player } />)}
+        </ul>
       </div>
+
     );
   }
 }
